a116e872715bd6a93007ce806a0581eaaf72c579
NatalyHSaez <mackahuaiquinao@gmail.com>
2025-06-13 00:38:35 -0400

    UpDate firebase.js
    Actualización de archivo firebase.js

--- accesspro/src/firebase.js
+++ accesspro/src/firebase.js
@@ -1,25 +1,18 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD08xAOXv_YMwqqNUz9aBj02Z8-Q5X2cg4",
-    authDomain: "accesspro-usuarios.firebaseapp.com",
-    projectId: "accesspro-usuarios",
-    storageBucket: "accesspro-usuarios.firebasestorage.app",  // corregido
-    messagingSenderId: "753755689011",
-    appId: "1:753755689011:web:9b90931c990bb8acb75346",
-    measurementId: "G-Y0H5X1HMPL"};
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
-// Servicios que vas a usar
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-  console.log("API Key:", process.env.REACT_APP_FIREBASE_API_KEY);
-
 
 export { auth, db };
